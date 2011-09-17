e040dcbea2227e94701cd090e4c26bab87390920
nandhini <nandhini.ramanujam@sedin.co.in>
2011-09-17 15:34:07 +0530

    application helper - text added

--- GIT_DEMO_APP/app/controllers/application_controller.rb
+++ GIT_DEMO_APP/app/controllers/application_controller.rb
@@ -1,18 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-ASDADASDASDASD
-ASFAF
-ASFA
-FASFA
 
-sdfsdf
-sdfsdf
-sdfdsf
-sdfsdf
-sdfsdf
 
-FASF
-
-cherry
+application_helper
 
 end
